6610b89a5ddd0cefe8f62ccd6cb76181f124b5cf
Hodoronk <tanasa.andrei@hotmail.com>
2023-08-30 22:44:31 +0300

    normal workflow

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { mainButtons, mainDivs } from "./dom" ;
 import {addTask} from "./addTask" ;
+import { todayChecker } from "./today" ;
+
 
 document.body.appendChild(mainDivs.leftDiv);
 document.body.appendChild(mainDivs.center);
@@ -8,5 +10,6 @@ mainDivs.leftDiv.appendChild (mainButtons.home) ;
 mainDivs.leftDiv.appendChild (mainButtons.today) ; 
 mainDivs.leftDiv.appendChild (mainButtons.week) ; 
 mainDivs.leftDiv.appendChild (mainButtons.addTask) ;
-
 mainButtons.addTask.addEventListener('click', addTask)
+mainButtons.today.addEventListener('click', todayChecker)
+
